4dabc35ce98825003176f1a984ef409e819a34f6
Spartak-Sargsyan <123450858+Spartak-Sargsyan@users.noreply.github.com>
2023-12-05 23:15:27 +0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
 // gdnel gumakr@
-// const arr = [1,2,3,[1,2],[1,[2,3,4],5],1]
-// type NestedArray = (number | number[] | NestedArray)[]
+const arr = [1,2,3,[1,2],[1,[2,3,4],5],1]
+type NestedArray = (number | number[] | NestedArray)[]
 
-// function sum(arr:NestedArray):number{
-//     return arr.reduce((acc,item)=>{
-//          if(Array.isArray(item)){
-//              return acc + sum(item)
-//          }
-//          else{
-//              return acc + item
-//          }
-//      },0)
-//     }
+function sum(arr:NestedArray):number{
+    return arr.reduce((acc,item)=>{
+         if(Array.isArray(item)){
+             return acc + sum(item)
+         }
+         else{
+             return acc + item
+         }
+     },0)
+    }
     
-//  const arrNumber:NestedArray  = [1,2,3,[1,2],[1,[2,3,4],5],1]
-//  const x = sum(arrNumber)
-//  console.log(x)
+ const arrNumber:NestedArray  = [1,2,3,[1,2],[1,[2,3,4],5],1]
+ const x = sum(arrNumber)
+ console.log(x)
 
 
-// binarni poisk rekursya
-// const arr1 = [5,7,34,65,75,83]
+binarni poisk rekursya
+const arr1 = [5,7,34,65,75,83]
 
 function binarySearchRecursive(arr:number[], k:number, left = 0, right = arr.length - 1) {
     if (left > right) {
